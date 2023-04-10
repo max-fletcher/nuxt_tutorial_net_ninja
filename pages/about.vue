be4d1e2855eddde610daa3d66916fd97256bcee4
Mahin Chowdhury <54007446+max-fletcher@users.noreply.github.com>
2023-04-10 22:52:07 +0600

    First commit. Done upto lesson 7. Started 8.

---
+++ pages/about.vue
@@ -0,0 +1,26 @@
+<template>
+   <div>
+      <h1>About</h1>
+      <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Natus doloribus dignissimos ut commodi, voluptas asperiores aliquid
+         nam amet pariatur ad magnam nisi illo laboriosam et, modi nobis necessitatibus quasi obcaecati!</p>
+      <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Tenetur enim quae voluptate. Molestiae quibusdam qui 
+         dolore maxime cupiditate enim repellat!</p>
+   </div>
+</template>
+
+<script>
+   export default {
+      
+   }
+</script>
+
+<style scoped>
+   h2{
+      margin-bottom: 20px;
+      font-size: 35px;
+   }
+
+   p{
+      margin: 20px 0px;
+   }
+</style>
---
+++ pages/products/[id].vue
@@ -0,0 +1,17 @@
+<template>
+   <div>
+      <h1>Products for {{ id }}</h1>
+   </div>
+</template>
+
+<script setup>
+   const { id } = useRoute().params
+
+   definePageMeta({
+      layout: 'products'
+   })
+</script>
+
+<style scoped>
+
+</style>
