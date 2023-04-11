92d108ea3ee9348e0f5052d52b21dadd3d1144ef
Mahin Chowdhury <54007446+max-fletcher@users.noreply.github.com>
2023-04-11 19:59:23 +0600

    Completed lesson_8.

---
+++ error.vue
@@ -0,0 +1,20 @@
+<template>
+   <div class="mt-7 max-w-sm mx-auto text-center card">
+      <p class="mt-7 text-7xl font-bold ">{{ error.statusCode }}</p>
+      <p class="mt-7 text-6xl">Oops</p>
+      <p class="mt-7">{{ error.message }}</p>
+      <button class="btn my-7" @click="handleClearError">Go Home</button>
+   </div>
+</template>
+
+<script setup>
+   defineProps(['error'])
+
+   // A function within a function. when the btn above is pressed, the "handleClearError" function is ran which in turn
+   // runs the "clearError" function. The object inside "clearError" redirects the user to home(i.e '/').
+   const handleClearError = () => clearError({ redirect: '/' })
+</script>
+
+<style lang="scss" scoped>
+
+</style>
--- pages/products/[id].vue
+++ pages/products/[id].vue
@@ -11,6 +11,12 @@
    // As a 2nd argument, the options object '{ key: id }' is passed so that the route param changes properly.
    const { data: product } = await useFetch(uri, { key: id })
 
+   // if no product exists, then go to error page with statusCode of 404 & statusMessage. "fatal" causes any Nuxtlink tags to
+   // throw error page if it matches a route but doesn't have valid route params.
+   if(!product.value){
+      throw createError({ statusCode: 404, statusMessage: "Product Not Found", fatal: true })
+   }
+
    definePageMeta({
       layout: 'products'
    })
